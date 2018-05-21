c75df8682633ea0db762fe0735a6b6ca5eb68e72
shaekhhasanshoron <shaekhhasanshoron@gmail.com>
2018-05-21 11:27:20 -0700

    Creating Repository with Spring Data JpaRepository

---
+++ src/main/java/com/shoron/demo/demoapplication/repository/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.shoron.demo.demoapplication.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shoron.demo.demoapplication.model.Note;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long>{
+
+}
