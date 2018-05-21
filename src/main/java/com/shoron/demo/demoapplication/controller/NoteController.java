082b014e184bfda67f9a870fac18ddafd56d724e
shaekhhasanshoron <shaekhhasanshoron@gmail.com>
2018-05-21 11:46:16 -0700

    Adding findAll and findById method for getting elemnts Using Spring Data

--- src/main/java/com/shoron/demo/demoapplication/controller/NoteController.java
+++ src/main/java/com/shoron/demo/demoapplication/controller/NoteController.java
@@ -1,9 +1,19 @@
 package com.shoron.demo.demoapplication.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shoron.demo.demoapplication.exception.ResourceNotFoundException;
+import com.shoron.demo.demoapplication.model.Note;
 import com.shoron.demo.demoapplication.repository.NoteRepository;
 
 @RestController
@@ -13,12 +23,22 @@ public class NoteController {
 	@Autowired
 	NoteRepository noteRepository;
 	
-    // Get All Notes	
+    // Get All Notes -> findAll()
+	@GetMapping("/notes")
+	public List<Note> getAllNotes() {
+	    return noteRepository.findAll();
+	}
+	
+    // Create a new Note 
 
-    // Create a new Note
-
-    // Get a Single Note
-
+	// Get a Single Note -> findById(noteId)
+	@GetMapping("/notes/{id}")
+	public Note getNoteById(@PathVariable(value = "id") Long noteId) {
+	    return noteRepository.findById(noteId)
+	            .orElseThrow(() -> new ResourceNotFoundException("Note", "id", noteId));
+	}
+	
+	
     // Update a Note
 
     // Delete a Note
